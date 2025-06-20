55557b6527b0e04edf67b2edddb532d848126312
ma4096 <derbauermlg@gmail.com>
2025-06-20 13:04:50 +0200

    Moved packages

--- README.md
+++ README.md
@@ -1,3 +1,5 @@
 # Billard sync API
 
-API hosted on the server to provide interactions between players at different locations and provide a interface for viewers
+API hosted on the server to provide interactions between players at different locations and provide a interface for viewers.
+
+Due to deployment, the packages are also directly included.
--- app.py
+++ app.py
@@ -1,10 +1,14 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, request, jsonify
 
 BillardAPI = Flask(__name__)
 
 @BillardAPI.route("/")
 def index():
     return render_template('index.html')
+
+@BillardAPI.route("/ask_for_game")
+def ask_for_game():
+    return jsonify({"state": 0})
 
 if __name__=="__main__":
     BillardAPI.run()
